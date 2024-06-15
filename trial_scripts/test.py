abc28ec62e1709d60dc3439632abf9eda8642b19
Bas Körver <bas_korver@outlook.com>
2024-06-15 13:21:33 +0200

    Added structlog
    TODO: prettyprint needs to be added when debugging

--- trial_scripts/test.py
+++ trial_scripts/test.py
@@ -1,3 +1,42 @@
-import torch
+"""
+Show how console logging looks like.
 
-print(torch.cuda.is_available())
+This is used for the screenshot in the readme and
+<https://www.structlog.org/en/stable/development.html>.
+"""
+
+from dataclasses import dataclass
+
+import structlog
+
+
+@dataclass
+class SomeClass:
+    x: int
+    y: str
+
+
+structlog.stdlib.recreate_defaults()  # so we have logger names
+
+log = structlog.get_logger("some_logger")
+
+log.debug("debugging is hard", a_list=[1, 2, 3])
+log.info("informative!", some_key="some_value")
+log.warning("uh-uh!")
+log.error("omg", a_dict={"a": 42, "b": "foo"})
+log.critical("wtf", what=SomeClass(x=1, y="z"))
+
+
+log2 = structlog.get_logger("another_logger")
+
+
+def make_call_stack_more_impressive():
+    try:
+        d = {"x": 42}
+        print(SomeClass(d["y"], "foo"))
+    except Exception:
+        log2.exception("poor me")
+    log.info("all better now!", stack_info=True)
+
+
+make_call_stack_more_impressive()
