d0fb5089cabe9b04814db6a2e64b949e63f70ccd
CedricCortenraede <CedricCortenraede@users.noreply.github.com>
2024-06-15 09:43:44 +0000

    Ran Black formatted on python code in ./src/
    There appeared to be some python formatting in c2325a86420c78de523d0e1e4b002bb70ffb1085 that did not conform with Black's formatting standards. So Black(https://github.com/psf/black) formatter was used to fix these issues.

--- src/api/api/db/postgres.py
+++ src/api/api/db/postgres.py
@@ -9,12 +9,11 @@ def create_tables(engine):
     # Import models.
     from models.country import Country
     from models.stream import StreamTag, Stream
-    
+
     # Import seeders.
     import db.seeders.country_seeder
     import db.seeders.stream_tag_seeder
     import db.seeders.stream_seeder
-    
+
     # Create tables.
     Base.metadata.create_all(engine)
-        
